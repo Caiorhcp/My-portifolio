565084c0c372879ea1e843115eb2bbd1ff638311
CaioCkll <caiorhcp12@gmail.com>
2024-04-28 14:59:44 -0300

    Create a interaction on buttons downloads and contacts

--- src/Pages/Home/sections/Hero/Hero.tsx
+++ src/Pages/Home/sections/Hero/Hero.tsx
@@ -53,7 +53,7 @@ const Hero= () => {
                     <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
                     <Grid item xs={12} md={4} display="flex" justifyContent="center">
                     
-                        <StyledButton>
+                        <StyledButton onClick={()=> console.log("download")}>
                         <DownloadIcon/>
                         <Typography>Download CV</Typography>
                         </StyledButton>
@@ -61,7 +61,7 @@ const Hero= () => {
                     </Grid>
                     
                     <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                    <StyledButton>
+                    <StyledButton onClick ={()=> console.log("contacts")}>
                         <EmailIcon/>
                         <Typography>Contact Me</Typography>
                         </StyledButton>
