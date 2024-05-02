eea7483696e65eb62cd5e0aea06c2e6ac33e1f95
CaioCkll <caiorhcp12@gmail.com>
2024-05-02 03:18:04 -0300

    add insighs vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import Home from "./Pages/Home/Home"
-
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 
 const App = () => {
 
 
   return (
-
+  <>
       <Home />
-
+      <SpeedInsights />
+      </>
   )
 }
 
