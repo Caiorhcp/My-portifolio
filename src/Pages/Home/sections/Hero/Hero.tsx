4a75a633a1e42545800bbb9ee8b56f56bb6185c6
CaioCkll <caiorhcp12@gmail.com>
2024-05-04 16:27:48 -0300

    3

--- src/Pages/Home/sections/Hero/Hero.tsx
+++ src/Pages/Home/sections/Hero/Hero.tsx
@@ -47,7 +47,7 @@ const Hero = () => {
 
     const handleDownloadClick = () => {
 
-        const pdfPath = '/src/CV/CaioGoncalves.pdf'; 
+        const pdfPath = '/src/assets/Images/CaioGoncalves.pdf'; 
 
         const publicUrl = `${window.location.origin}${pdfPath}`;
     
